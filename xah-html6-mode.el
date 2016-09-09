baa75ab0ee849c0279d0592010dc8569fe318944
Xah Lee <xah@xahlee.org>
2016-09-08 21:19:28 -0700

    stuff

--- xah-html6-mode.el
+++ xah-html6-mode.el
@@ -3,20 +3,20 @@
 ;; http://xahlee.info/comp/html6.html
 
 ;;; HISTORY
-;; version 0.2, 2011-05-27
+;; version 0.3, 2016-09-08
 ;; version 0.1, 2010-12-17
 
-(defvar html6-font-lock-defaults nil "Value for font-lock-defaults.")
+(defvar xah-html6-font-lock-defaults nil "Value for font-lock-defaults.")
 
-(setq html6-font-lock-defaults
+(setq xah-html6-font-lock-defaults
       '(("〔\\([^ ]+?\\) " . (1 font-lock-function-name-face))
         ("“\\([^ ]+?\\)”" . (1 font-lock-string-face))
         ("「\\([^」]+\\)」" . (1 font-lock-variable-name-face))
         ) )
 
 (define-derived-mode xah-html6-mode fundamental-mode
-  (setq font-lock-defaults '(html6-font-lock-defaults))
+  (setq font-lock-defaults '(xah-html6-font-lock-defaults))
   (setq mode-name "HTML6")
   )
 
-(provide 'html6)
+(provide 'xah-html6)
